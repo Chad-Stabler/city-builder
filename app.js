243896cd2acfc6bba35dfa75212dd25b72654033
Chad Stabler <stablerpsn@gmail.com>
2022-04-28 13:14:07 -0500

    finished second selector, small style edits

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 // import functions and grab DOM elements
 const climateEl = document.getElementById('climate-select');
 const climateImg = document.getElementById('climate');
+const typeEl = document.getElementById('type-select');
+const typeImg = document.getElementById('type');
 // let state
 
 // set event listeners 
 climateEl.addEventListener('change', () => {
     climateImg.style.backgroundImage = `url(../assets/${climateEl.value}.png)`;
+});
+
+typeEl.addEventListener('change', () => {
+    typeImg.style.backgroundImage = `url(../assets/${typeEl.value}.png)`;
 });
   // get user input
   // use user input to update state 

67243ef9008ce66c73fad4d1a6c62841bebb46eb
Chad Stabler <stablerpsn@gmail.com>
2022-04-28 12:56:37 -0500

    finished first selector with 4 images

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // import functions and grab DOM elements
-
+const climateEl = document.getElementById('climate-select');
+const climateImg = document.getElementById('climate');
 // let state
 
 // set event listeners 
+climateEl.addEventListener('change', () => {
+    climateImg.style.backgroundImage = `url(../assets/${climateEl.value}.png)`;
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state

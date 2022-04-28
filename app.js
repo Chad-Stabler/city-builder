23de5987a5826e47d38aee493c8bae6d366ceddc
Chad Stabler <stablerpsn@gmail.com>
2022-04-28 13:48:34 -0500

    finished displayStats function, more small styling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const typeEl = document.getElementById('type-select');
 const typeImg = document.getElementById('type');
 const castleEL = document.getElementById('castle-select');
 const castleImg = document.getElementById('castle');
+const statEl = document.getElementById('stat-display');
 // let state
 let climateChange = 0;
 let typeChange = 0;
@@ -13,20 +14,23 @@ let castleChange = 0;
 climateEl.addEventListener('change', () => {
     climateChange++;
     climateImg.style.backgroundImage = `url(../assets/${climateEl.value}.png)`;
+    displayStats();
 });
 
 typeEl.addEventListener('change', () => {
     typeChange++;
     typeImg.style.backgroundImage = `url(../assets/${typeEl.value}.png)`;
+    displayStats();
 });
 
 castleEL.addEventListener('change', () => {
     castleChange++;
     castleImg.style.backgroundImage = `url(../assets/${castleEL.value}.png)`;
+    displayStats();
 });
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 function displayStats() {
-
+    statEl.textContent = `Climate changed ${climateChange} times, population type changed ${typeChange} times, castle changed ${castleChange} times`;
 }

76240b337027eff855a2dde10f25864a15e1eba5
Chad Stabler <stablerpsn@gmail.com>
2022-04-28 14:10:18 -0500

    finished displaySlogans, finished project other than styling choices for later

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ const typeImg = document.getElementById('type');
 const castleEL = document.getElementById('castle-select');
 const castleImg = document.getElementById('castle');
 const statEl = document.getElementById('stat-display');
+const add = document.getElementById('addBtn');
+const inputEl = document.getElementById('user-input');
+const sloganDis = document.getElementById('slogan-display');
 // let state
 let climateChange = 0;
 let typeChange = 0;
 let castleChange = 0;
+let slogans = [];
 // set event listeners 
 climateEl.addEventListener('change', () => {
     climateChange++;
@@ -28,9 +32,24 @@ castleEL.addEventListener('change', () => {
     castleImg.style.backgroundImage = `url(../assets/${castleEL.value}.png)`;
     displayStats();
 });
+
+add.addEventListener('click', () => {
+    slogans.push(inputEl.value);
+    console.log(slogans);
+    inputEl.value = '';
+    displaySlogans();
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 function displayStats() {
     statEl.textContent = `Climate changed ${climateChange} times, population type changed ${typeChange} times, castle changed ${castleChange} times`;
+}
+function displaySlogans() {
+    sloganDis.textContent = '';
+    for (let slogan of slogans) {
+        const newP = document.createElement('p');
+        newP.textContent = slogan;
+        sloganDis.append(newP);
+    }
 }
